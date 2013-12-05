00d9e1097569a4563a5d22f9e142dca2e77fd113
tnog <takahiro.noguchi@gmail.com>
2013-12-05 14:08:25 -0800

    Updated GPL license

--- sketchbook.php
+++ sketchbook.php
@@ -23,7 +23,7 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
  *     Redistributions in binary form must reproduce the above copyright notice,
  *         this list of conditions and the following disclaimer in the documentation
  *         and/or other materials provided with the distribution.
- *     Neither the name of Ales Loziak nor the names of its
+ *     Neither the name of Takahiro Noguchi nor the names of its
  *         contributors may be used to endorse or promote products derived from this
  *         software without specific prior written permission.
  * 
